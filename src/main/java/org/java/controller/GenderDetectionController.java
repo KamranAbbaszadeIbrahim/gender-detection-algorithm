9cade1e3ead62f441df43b9bd87816a88ba21e2d
Kamran Abbaszade <kiabbaszade@gmail.com>
2021-02-15 02:25:24 +0100

    Fixed GenderDetectionController class

--- src/main/java/org/java/controller/GenderDetectionController.java
+++ src/main/java/org/java/controller/GenderDetectionController.java
@@ -1,11 +1,13 @@
 package org.java.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.java.model.RequestDto;
 import org.java.model.ResourceDto;
 import org.java.model.ResponseDto;
 import org.java.service.GenderDetectionService;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping
 public class GenderDetectionController {
@@ -17,11 +19,13 @@ public class GenderDetectionController {
 
     @GetMapping("/resource")
     public ResourceDto getListOfNames(){
+        log.debug("Class: GenderDetectionController, method: getListOfNames");
         return genderDetectionService.getListOfNames();
     }
 
     @PostMapping("/request")
     public ResponseDto detectGender(@RequestBody RequestDto requestDto){
+        log.debug("Class: GenderDetectionController, method: detectGender");
         return genderDetectionService.detectGender(requestDto);
     }
 }
